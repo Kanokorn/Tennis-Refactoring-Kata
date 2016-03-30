84601e77abb2913af38f58884752d6cd70836a71
Kanokon Chongnguluam <hippo.ahamayba@gmail.com>
2016-03-30 16:57:23 +0700

    Refactor code use map

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -10,26 +10,20 @@ TennisGame1.prototype.wonPoint = function(playerName) {
         this.m_score1 += 1;
     else
         this.m_score2 += 1;
+
 };
 
 TennisGame1.prototype.getScore = function() {
     var score = "";
     var tempScore = 0;
+    var mapScore = {
+        0: "Love-All",
+        1: "Fifteen-All",
+        2: "Thirty-All"
+    };
+
     if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
+        score = mapScore[this.m_score1] || "Deuce";
     } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
         var minusResult = this.m_score1 - this.m_score2;
         if (minusResult === 1) score = "Advantage player1";
